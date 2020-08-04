4da59d13cd11e2bd9100522ac3921ca583a6033d
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-08-04 21:50:48 +0900

    アクティブストレージの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,20 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    @item.save
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:text,:price,:genre_id,:status_id,:bunder_id,:shipment_id,:area_id,:image)
+  end
+
 end

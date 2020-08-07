0d0030bf344805779395568cfacb7a04f35bea50
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-08-07 16:54:20 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   get '/users/sign_out' => 'devise/sessions#destroy'
-  resources :items, only: [:index,:new,:create,:show,:edit,:update]
+  resources :items
 end

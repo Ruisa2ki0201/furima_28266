0a3070025f48da76e7e84d67e7dd85f1861dfcc0
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-08-09 15:42:55 +0900

    sould autの表示非表示の実装

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -3,14 +3,6 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_cod, :shipment_id, :cities, :address, :build_name, :phone_number, :user_id, :item_id, :token
   
-  with_options presence: true do
-    validates :postal_cod, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'は-を含む7桁で入力してください'}
-    validates :shipment_id
-    validates :cities
-    validates :address
-    validates :build_name
-    validates :phone_number
-  end
 
   def save
     Order.create(user_id: user_id, item_id: item_id)

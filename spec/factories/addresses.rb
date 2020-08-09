d6bee7d68af0cc09cdc878c71aca297146e4447a
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-08-09 15:42:18 +0900

    単体テストコード

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_cod          {"123-4567"}
+    shipment_id         { 3 } 
+    cities              {"横浜市緑区"}
+    address             {"青山1-1"}
+    build_name          {"柳ビル103"}
+    phone_number        {"09012345678"}              
   end
 end
